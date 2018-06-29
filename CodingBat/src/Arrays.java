a82d206d976f8e4660aba06de2b32c3ddffd7501
Daniel Jenkins <dan_jen1@protonmail.com>
2018-06-28 20:48:14 -0600

    update

--- CodingBat/src/Arrays.java
+++ CodingBat/src/Arrays.java
@@ -297,5 +297,37 @@ public class Arrays
             return new int[]{nums[0], nums[1]};
         }
     }
+
+    public boolean unlucky1(int[] nums)
+    {
+        if (nums.length < 2)
+        {
+            return false;
+        } else if (nums.length > 2)
+        {
+            if ((nums[0] == 1 && nums[1] == 3) || nums[1] == 1 && nums[2] == 3)
+            {
+                return true;
+            } else if (nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3)
+                    ||
+            {
+                return true;
+            }
+        } else if (nums.length == 2 && nums [0] == 1 && nums[1] == 3)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        Arrays newOne = new Arrays();
+        int[] one = {1, 1, 3, 1};
+        System.out.println(newOne.unlucky1(one));
+
+
+    }
+
 }
 
