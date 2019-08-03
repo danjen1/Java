2c0e8cedcefa9352a6c3b15ffc32c8cfdf1ecd03
Daniel Jenkins <dan_jen9@icloud.com>
2019-08-02 18:57:42 -0600

    updated file / folder names

--- mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
@@ -1,8 +1,7 @@
-
 public class Smileys {
-
+ 
     public static void main(String[] args) {
-
+ 
         // Test your method at least with the following
         printWithSmileys("Meth");
         printWithSmileys("Bee");
@@ -13,13 +12,13 @@ public class Smileys {
         printWithSmileys("12345678");
         printWithSmileys("1234567");
     }
-
+ 
     private static void printWithSmileys(String characterString) {
         int length = characterString.length();
         int termEven = length + 1;
         int termOdd = length + 2;
         String smiley = ":)";
-
+ 
         if (length % 2 == 0) {
             termEven = (length + 6) / 2;
             printSmiley(termEven);
@@ -27,7 +26,7 @@ public class Smileys {
             System.out.println(smiley + " " + characterString + " " + smiley);
             printSmiley(termEven);
         }
-
+ 
         if (length % 2 == 1) {
             termOdd = (length + 7) / 2;
             printSmiley(termOdd);
@@ -36,7 +35,7 @@ public class Smileys {
             printSmiley(termOdd);
         }
     }
-
+ 
     private static void printSmiley(int length) {
         String smiley = ":)";
         for (int i = 0; i < length; i++) {
@@ -45,40 +44,3 @@ public class Smileys {
         System.out.println("");
     }
 }
-/*print("Daniel is amazing", 3);
-        
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter characterString: ");
-        String characterString = in.nextLine();
-        
-        printStars(characterString.length());
-        System.out.println(characterString);
-        printStars(characterString.length());
-    
-        Book senseAndSensibility = new Book("Sense and Sensibility", 1811);
-        Book prideAndPrejudice = new Book("Pride and Prejudice", 1813);
-        
-        System.out.println(senseAndSensibility.getName());
-        System.out.println(prideAndPrejudice.getName());
-        
-        Scanner in = new Scanner(System.in);
-        UserInterface inter = new UserInterface(in);
-        
-        inter.start();
-        
-    }
-
-    public static void print(String characterString, int times) {
-        for (int i = 0; i < times; i++) {
-            System.out.println(characterString);
-        }
-    }
-    
-    public static void printStars(int times){
-        String star = "*";
-        for (int i = 1; i < times; i++){
-            System.out.print(star);
-        }
-        System.out.println();
-    }
- */
